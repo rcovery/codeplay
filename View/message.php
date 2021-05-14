49ceb75f77bede75654ff8e80c25bce799fa0a9d
rcovery <ryanczpereira@gmail.com>
2021-05-13 22:16:06 -0300

    chore: change view

--- View/message.php
+++ View/message.php
@@ -17,6 +17,16 @@ class View
         $this->render();
     }
 
+    public function success(){
+        $this->message = "
+        <div class='message_box' onclick='destroy_message(this)'>
+            <p class='success message'>{$this->message}</p>
+        </div>
+        ";
+
+        $this->render();
+    }
+
     public function render()
     {
         echo $this->message;
