aea863c4ff1a68e2346bd65f6677b76c50ce568e
rcovery <ryanczpereira@gmail.com>
2021-05-16 02:20:24 -0300

    chore: source files

--- index.php
+++ index.php
@@ -8,10 +8,104 @@
     <title>CodePlay :: Página principal</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="assets/css/global.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/navbar.css">
 </head>
 
 <body>
     <?php include("View/navbar.php"); ?>
+    <div class="header_landing">
+    	<div>
+    		<span class="gradient_line"></span>
+	    	<span class="flex_block emoji">&#10024;
+                <p class="header_text italic_text">&nbsp;somos a&nbsp;</p>
+            &#10024;</span>
+            <div class="cshadow relative_block">
+                <p class="header_text pixel custom_shadow">CODEPLAY</p>
+	    	    <p class="header_text pixel" style="position: relative;">CODEPLAY</p>
+            </div>
+	    	<span class="gradient_line"></span>
+	    </div>
+    </div>
+
+    <div class="block">
+        <h1>Venha ser um gamer!</h1>
+        <p>A Codeplay é uma plataforma de ensino de programação de jogos digitais!</p>
+    </div>
+
+    <div class="block reverse">
+        <h1>O que você vai aprender</h1>
+        <div class="showcase">
+          <div class="game_item">
+            <img src="assets/images/default_pic.png">
+            <p>"Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus br..."</p>
+            <a class="gradient_button">JOGAR AGORA</a>
+          </div>
+          <div class="game_item">
+            <img src="assets/images/default_pic.png">
+            <p>"Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus br..."</p>
+            <a class="gradient_button">JOGAR AGORA</a>
+          </div>
+          <div class="game_item">
+            <img src="assets/images/default_pic.png">
+            <p>"Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus br..."</p>
+            <a class="gradient_button">JOGAR AGORA</a>
+          </div>
+        </div>
+    </div>
+
+    <div class="block">
+        <h1>Quem somos?</h1>
+        <div class="flex_block">
+            <img src="assets/images/default_pic.png">
+            <div class="who_quote">
+                <p>"Aqui é Body Builder Ipsum! Eu quero esse 13 daqui a pouquinho aí. Tá saindo da jaula o monstro! Sabe o que é isso daí? Trapézio descendente é o nome disso aí. É verão o ano todo vem cumpadi. Aqui nóis constrói fibra, não é água com músculo. Não vai dá não. É esse que a gente quer, é ele que nóis vamo buscar. Boraaa, Hora do Show."
+                </p>
+                <p>"Brownie topping chocolate powder caramels dragée. Chupa chups tiramisu pastry jelly beans jelly beans chocolate pastry. Macaroon chupa chups donut lollipop apple pie. Jujubes tart candy canes soufflé."
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="block quote reverse">
+        <span class="quote_icon">
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 475.1 475.1" style="enable-background:new 0 0 475.1 475.1;" xml:space="preserve"><path class="st0" d="M164.4,219.3h-64c-7.6,0-14.1-2.7-19.4-8c-5.3-5.3-8-11.8-8-19.4v-9.1c0-20.2,7.1-37.4,21.4-51.7
+    c14.3-14.3,31.5-21.4,51.7-21.4h18.3c4.9,0,9.2-1.8,12.8-5.4c3.6-3.6,5.4-7.9,5.4-12.8V54.8c0-4.9-1.8-9.2-5.4-12.8
+    c-3.6-3.6-7.9-5.4-12.8-5.4h-18.3c-19.8,0-38.7,3.9-56.7,11.6c-18,7.7-33.5,18.1-46.7,31.3c-13.1,13.1-23.6,28.7-31.3,46.7
+    C3.9,144,0,162.9,0,182.7v201c0,15.2,5.3,28.2,16,38.8c10.7,10.7,23.6,16,38.8,16h109.6c15.2,0,28.2-5.3,38.8-16
+    c10.7-10.7,16-23.6,16-38.8V274.1c0-15.2-5.3-28.2-16-38.8C192.6,224.6,179.7,219.3,164.4,219.3z M459.1,235.3
+    c-10.7-10.7-23.6-16-38.8-16h-64c-7.6,0-14.1-2.7-19.4-8c-5.3-5.3-8-11.8-8-19.4v-9.1c0-20.2,7.1-37.4,21.4-51.7
+    c14.3-14.3,31.5-21.4,51.7-21.4h18.3c4.9,0,9.2-1.8,12.8-5.4c3.6-3.6,5.4-7.9,5.4-12.8V54.8c0-4.9-1.8-9.2-5.4-12.8
+    c-3.6-3.6-7.9-5.4-12.8-5.4H402c-19.8,0-38.7,3.9-56.7,11.6c-18,7.7-33.5,18.1-46.7,31.3c-13.1,13.1-23.6,28.7-31.3,46.7
+    c-7.7,18-11.6,36.9-11.6,56.7v201c0,15.2,5.3,28.2,16,38.8c10.7,10.7,23.6,16,38.8,16h109.6c15.2,0,28.2-5.3,38.8-16 c10.7-10.7,16-23.6,16-38.8V274.1C475.1,258.9,469.8,245.9,459.1,235.3z"/>
+            </svg>
+        </span>
+        <p><b>Prepare for Pokem! Make it Ipsum! To protect the world from devastation!</b></p>
+        <p><b>Prepare for Pokem! Make it Ipsum! To protect the world from devastation!</b></p>
+        <p><b>Prepare for Pokem! Make it Ipsum! To protect the world from devastation!</b></p>
+        <p><b>Prepare for Pokem! Make it Ipsum! To protect the world from devastation!</b></p>
+        <span class="quote_icon flipped">
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 475.1 475.1" style="enable-background:new 0 0 475.1 475.1;" xml:space="preserve"><path class="st0" d="M164.4,219.3h-64c-7.6,0-14.1-2.7-19.4-8c-5.3-5.3-8-11.8-8-19.4v-9.1c0-20.2,7.1-37.4,21.4-51.7
+    c14.3-14.3,31.5-21.4,51.7-21.4h18.3c4.9,0,9.2-1.8,12.8-5.4c3.6-3.6,5.4-7.9,5.4-12.8V54.8c0-4.9-1.8-9.2-5.4-12.8
+    c-3.6-3.6-7.9-5.4-12.8-5.4h-18.3c-19.8,0-38.7,3.9-56.7,11.6c-18,7.7-33.5,18.1-46.7,31.3c-13.1,13.1-23.6,28.7-31.3,46.7
+    C3.9,144,0,162.9,0,182.7v201c0,15.2,5.3,28.2,16,38.8c10.7,10.7,23.6,16,38.8,16h109.6c15.2,0,28.2-5.3,38.8-16
+    c10.7-10.7,16-23.6,16-38.8V274.1c0-15.2-5.3-28.2-16-38.8C192.6,224.6,179.7,219.3,164.4,219.3z M459.1,235.3
+    c-10.7-10.7-23.6-16-38.8-16h-64c-7.6,0-14.1-2.7-19.4-8c-5.3-5.3-8-11.8-8-19.4v-9.1c0-20.2,7.1-37.4,21.4-51.7
+    c14.3-14.3,31.5-21.4,51.7-21.4h18.3c4.9,0,9.2-1.8,12.8-5.4c3.6-3.6,5.4-7.9,5.4-12.8V54.8c0-4.9-1.8-9.2-5.4-12.8
+    c-3.6-3.6-7.9-5.4-12.8-5.4H402c-19.8,0-38.7,3.9-56.7,11.6c-18,7.7-33.5,18.1-46.7,31.3c-13.1,13.1-23.6,28.7-31.3,46.7
+    c-7.7,18-11.6,36.9-11.6,56.7v201c0,15.2,5.3,28.2,16,38.8c10.7,10.7,23.6,16,38.8,16h109.6c15.2,0,28.2-5.3,38.8-16 c10.7-10.7,16-23.6,16-38.8V274.1C475.1,258.9,469.8,245.9,459.1,235.3z"/>
+            </svg>
+        </span>
+    </div>
+
+    <div class="block">
+        <h1>Contato</h1>
+        form
+    </div>
+
+    <footer>
+        foooter end
+    </footer>
 
     <script src="assets/js/script.js"></script>
 </body>
