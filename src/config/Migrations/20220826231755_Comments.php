74ef37ea1946f37f76615e7a1efce01c717a478b
RCovery <ryanczpereira@gmail.com>
2022-08-26 21:37:43 -0300

    feat: default value migrations

---
+++ src/config/Migrations/20220826231755_Comments.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class Comments extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('comments');
+        $table->addColumn('user_id', 'integer', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('content', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
