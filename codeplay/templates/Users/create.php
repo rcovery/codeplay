cbb362c9f70a5cb35090bc534552d3c344c80d6c
RCovery <ryanczpereira@gmail.com>
2022-09-03 00:29:39 -0300

    feat: create user form

--- codeplay/templates/Users/create.php
+++ codeplay/templates/Users/create.php
@@ -1,5 +1,18 @@
-<?php $this->assign('title', 'Signup'); ?>
+<div>
+    <?= $this->Form->create(); ?>
+        <input hidden name="token" value="<?= $this->request->getAttribute('csrfToken'); ?>"/>
+        <label for="name_input">Nome Completo</label>
+        <input id="name_input" name='name' required/>
+        <label for="username_input">Nome do usuário</label>
+        <input id="username_input" name='username' required/>
+        <label for="email_input">Email</label>
+        <input id="email_input" name='email' type="email" required/>
+        <label for="password_input">Senha</label>
+        <input id="password_input" name='password' type="password" required/>
 
-<form method='post'>
-    Teste label
-</form>
+        <label for="consent_input">Aceito os termos de uso</label>
+        <input id="consent_input" name='consent' type="checkbox" required/>
+        
+        <input type="submit" value="Criar sua conta!"/>
+    </form>
+</div>
