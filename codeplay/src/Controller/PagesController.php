0d993e81980e8fc98324b4cb7c9d66ebb433e01e
RCovery <ryanczpereira@gmail.com>
2022-09-03 12:49:08 -0300

    feat: create home file

--- codeplay/src/Controller/PagesController.php
+++ codeplay/src/Controller/PagesController.php
@@ -31,18 +31,6 @@ use Cake\View\Exception\MissingTemplateException;
  */
 class PagesController extends AppController
 {
-    /**
-     * Displays a view
-     *
-     * @param string ...$path Path segments.
-     * @return \Cake\Http\Response|null
-     * @throws \Cake\Http\Exception\ForbiddenException When a directory traversal attempt.
-     * @throws \Cake\View\Exception\MissingTemplateException When the view file could not
-     *   be found and in debug mode.
-     * @throws \Cake\Http\Exception\NotFoundException When the view file could not
-     *   be found and not in debug mode.
-     * @throws \Cake\View\Exception\MissingTemplateException In debug mode.
-     */
     public function display(string ...$path): ?Response
     {
         if (!$path) {
@@ -69,5 +57,9 @@ class PagesController extends AppController
             }
             throw new NotFoundException();
         }
+    }
+
+    public function home() {
+        return $this->render();
     }
 }
