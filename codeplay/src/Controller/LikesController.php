4b4cdc05cc30cec7cdac709de583823098e2e553
RCovery <ryanczpereira@gmail.com>
2022-10-04 20:45:53 -0300

    fix: test cases name

--- codeplay/src/Controller/LikesController.php
+++ codeplay/src/Controller/LikesController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-class UsersController extends AppController
+class LikesController extends AppController
 {
     // Give a star to post
     public function likePost()
